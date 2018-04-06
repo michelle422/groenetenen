339de0a996dce116c969f8b8616e5dfbb64152b1
michelle422 <kazekage422@gmail.com>
2018-04-06 11:52:25 +0200

    commit met session scoped beans

---
+++ src/main/java/be/vdab/web/DefaultVoorkeur.java
@@ -0,0 +1,21 @@
+package be.vdab.web;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+class DefaultVoorkeur implements Voorkeur, Serializable {
+	private static final long serialVersionUID = 1L;
+	private String foto;
+	
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+}
---
+++ src/main/java/be/vdab/web/Voorkeur.java
@@ -0,0 +1,6 @@
+package be.vdab.web;
+
+interface Voorkeur {
+	void setFoto(String foto);
+	String getFoto();
+}
